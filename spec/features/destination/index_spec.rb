f2125a818b55711e81457a12ce575ac3918e66d9
Karan Mehta <karanm645@gmail.com>
2022-03-03 18:17:42 -0600

    create destination index page

---
+++ spec/features/destination/index_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper' 
+
+RSpec.describe "Destination Index Page" do 
+  before :each do 
+    @destination_1 = Destination.create!(name: "Chadmouth", zip: "26626-3209", description: "Funny", image_url: "https://via.placeholder.com/300x300.png")
+  end 
+  describe 'when i visit the index page' do 
+    it 'has a button to click on a destination' do 
+      visit "/"
+      click_on "Show"
+      expect(current_path).to eq("/destinations/#{@destination_1.id}")
+    end 
+  end 
+end
